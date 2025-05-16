0d8e94af071fb77cdf3dcfdaeb95938c1f8ace5b
Chi <cheez2012@gmail.com>
2025-05-17 03:37:05 +0700

    next config change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  transpilePackages: [
+    "@react-pdf/renderer",
+    "react-pdf-tailwind",
+  ],
   /* config options here */
 };
 
